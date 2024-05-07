332416e265cbe1e9ac0d7fb5d094e6b6c47434c8
rajorokit <rajorokit6654@gmail.com>
2024-05-08 03:57:09 +0500

    Adding Tahira OOP My Bank Project

---
+++ index.ts
@@ -0,0 +1,170 @@
+#! /usr/bin/env node
+import  inquirer from "inquirer";
+
+export class Transaction {
+    type: string;
+    amount: number;
+    date: Date;
+
+    constructor(type: string, amount: number) {
+        this.type = type;
+        this.amount = amount;
+        this.date = new Date();
+    }
+}
+
+export class Customer {
+    static accountCount: number = 1000;
+    accountNumber: number;
+    name: string;
+    balance: number;
+    transactions: Transaction[];
+
+    constructor(name: string, initialBalance: number) {
+        this.accountNumber = Customer.accountCount++;
+        this.name = name;
+        this.balance = initialBalance;
+        this.transactions = [new Transaction("Initial Deposit", initialBalance)];
+    }
+
+    deposit(amount: number) {
+        this.balance += amount;
+        this.transactions.push(new Transaction("Deposit", amount));
+    }
+
+    withdraw(amount: number) {
+        this.balance -= amount;
+        this.transactions.push(new Transaction("Withdraw", amount));
+    }
+
+    showTransactions() {
+        this.transactions.forEach(transaction => {
+            console.log(`${transaction.type}: ${transaction.amount}`);
+        });
+    }
+}
+
+const customers: Customer[] = [];
+
+const mainMenu = async () => {
+    const options = [
+        "Create Account",
+        "Check Balance",
+        "Deposit",
+        "Withdraw",
+        "Transaction History",
+        "Exit"
+    ];
+
+    const answer = await inquirer.prompt({
+        type: "list",
+        message: "Select an option:",
+        name: "option",
+        choices: options
+    });
+
+    switch (answer.option) {
+        case "Create Account":
+            await createAccount();
+            break;
+        case "Check Balance":
+            await checkBalance();
+            break;
+        case "Deposit":
+            await deposit();
+            break;
+        case "Withdraw":
+            await withdraw();
+            break;
+        case "Transaction History":
+            await transactionHistory();
+            break;
+        case "Exit":
+            console.log("Thank you for using our bank services. Goodbye!");
+            process.exit();
+    }
+};
+
+const createAccount = async () => {
+    const response = await inquirer.prompt([
+        { type: "input", message: "Enter your name:", name: "name" },
+        { type: "number", message: "Enter initial deposit amount:", name: "balance" }
+    ]);
+    const customer = new Customer(response.name, response.balance);
+    customers.push(customer);
+    console.log(`Account created successfully. Your account number is ${customer.accountNumber}.`);
+    mainMenu();
+};
+
+const checkBalance = async () => {
+    const accountNumber = await getAccountNumber();
+    const customer = customers.find(c => c.accountNumber === accountNumber);
+    if (customer) {
+        console.log(`Your current balance is: ${customer.balance}`);
+    } else {
+        console.log("Invalid account number.");
+    }
+    mainMenu();
+};
+
+const deposit = async () => {
+    const accountNumber = await getAccountNumber();
+    const customer = customers.find(c => c.accountNumber === accountNumber);
+    if (customer) {
+        const amount = await getAmount("deposit");
+        customer.deposit(amount);
+        console.log(`Amount deposited successfully. new balance is: ${customer.balance}`);
+    } else {
+        console.log("Invalid account number.");
+    }
+    mainMenu();
+};
+
+const withdraw = async () => {
+    const accountNumber = await getAccountNumber();
+    const customer = customers.find(c => c.accountNumber === accountNumber);
+    if (customer) {
+        const amount = await getAmount("withdraw");
+        if (customer.balance >= amount) {
+            customer.withdraw(amount);
+            console.log(`Amount withdrawn successfully. new balance is: ${customer.balance}`);
+        } else {
+            console.log("Insufficient funds.");
+        }
+    } else {
+        console.log("Invalid account number.");
+    }
+    mainMenu();
+};
+
+const transactionHistory = async () => {
+    const accountNumber = await getAccountNumber();
+    const customer = customers.find(c => c.accountNumber === accountNumber);
+    if (customer) {
+        console.log("Transaction History:");
+        customer.showTransactions();
+    } else {
+        console.log("Invalid account number.");
+    }
+    mainMenu();
+};
+
+const getAccountNumber = async () => {
+    const response = await inquirer.prompt({
+        type: "input",
+        message: "Enter your account number:",
+        name: "accountNumber"
+    });
+    return parseInt(response.accountNumber);
+};
+
+const getAmount = async (action: string) => {
+    const response = await inquirer.prompt({
+        type: "number",
+        message: `Enter amount to ${action}:`,
+        name: "amount"
+    });
+    return response.amount;
+};
+
+mainMenu();
